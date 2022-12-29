e6dfee5f6d2352eb8547550a95aaaa39e0e678d3
4kills <dominik.ochs@gmx.net>
2022-12-29 18:26:57 +0100

    finished weitere prinzipien

--- documentation/Quellcode/dfs.cpp
+++
@@ -1,8 +0,0 @@
-void Graph::DFS(int v) {
-    visited[v] = true;
-    
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
-            DFS(*i);
-}
