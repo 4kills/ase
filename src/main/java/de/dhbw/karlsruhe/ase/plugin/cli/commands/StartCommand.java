827cd8434900ac29405afde964b7800f5652bbc4
4kills <dominik.ochs@gmx.net>
2022-12-31 18:58:29 +0100

    replace temp with query for card deck in StartCommand

--- src/main/java/de/dhbw/karlsruhe/ase/plugin/cli/commands/StartCommand.java
+++ src/main/java/de/dhbw/karlsruhe/ase/plugin/cli/commands/StartCommand.java
@@ -20,6 +20,23 @@ public record StartCommand(List<Card> cards) implements Command {
 
     @Override
     public void execute(final Game game) {
+        try {
+            game.start(deckFrom(cards));
+        } catch (final IllegalActionException e) {
+            new ErrorBuilder("starting game due to " + e.getMessage(),
+                    "check whether your command is semantically correct").print();
+            return;
+        } catch (final GameStatusException e) {
+            new ErrorBuilder("game is running, can not use start now", "perhaps you want to reset").print();
+            return;
+        }
+        Terminal.printLine(CommonOutput.OK);
+    }
+
+    /**
+     * Returns a new CardDeck created from cards or a random deck if cards is empty
+     */
+    private CardDeck deckFrom(List<Card> cards) {
         final CardDeck deck;
 
         if (cards.isEmpty()) {
@@ -32,16 +49,6 @@ public record StartCommand(List<Card> cards) implements Command {
             }
         }
 
-        try {
-            game.start(deck);
-        } catch (final IllegalActionException e) {
-            new ErrorBuilder("starting game due to " + e.getMessage(),
-                    "check whether your command is semantically correct").print();
-            return;
-        } catch (final GameStatusException e) {
-            new ErrorBuilder("game is running, can not use start now", "perhaps you want to reset").print();
-            return;
-        }
-        Terminal.printLine(CommonOutput.OK);
+        return deck;
     }
 }
